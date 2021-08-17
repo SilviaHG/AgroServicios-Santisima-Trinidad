f55194409970aa599b15109dd20e25c8d1be9b00
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-08-17 14:51:41 -0600

    Import routes.js and router-view added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
     <BarraNavegacion></BarraNavegacion>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Bienvenida al sitio web de Agroservicios Santisima Trinidad en Palmira"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import BarraNavegacion from './components/BarraNavegacion.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     BarraNavegacion
   }
 
@@ -21,14 +18,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 @import '~bootstrap/dist/css/bootstrap.css'
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/js/bootstrap.bundle'
+import router from './routes.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
