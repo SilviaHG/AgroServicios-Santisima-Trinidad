b380326819019eae6f340db17228fa957bc8cdf3
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-08-17 14:51:09 -0600

    New file routes.js to register all the routes

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+
+    routes: [
+        {
+            path: '/',
+            name: 'inicio',
+            component: require('./components/inicio/Inicio').default,
+        },
+        {
+            path: '/productos-veterinaria',
+            name: 'veterinaria',
+            component: require('./components/inicio/Inicio').default,
+        },
+        {
+            path: '/productos-construccion',
+            name: 'construccion',
+            component: require('./components/inicio/Inicio').default,
+        },
+        {
+            path: '/contactenos',
+            name: 'contactenos',
+            component: require('./components/inicio/Inicio').default,
+        },
+    ],
+
+    mode: 'history',
+});
