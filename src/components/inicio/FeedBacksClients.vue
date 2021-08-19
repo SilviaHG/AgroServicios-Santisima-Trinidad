4c2714373b9487a7b128c988dd4c291e2c04165d
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-08-19 14:37:56 -0600

    Mejoras graficas para los feedbacks

---
+++ src/components/inicio/FeedBacksClients.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="feedback-clients" class="container-fluid">
+        <div class="row">
+
+            <div class="col-4">
+                <div class="card p-3">
+                    <figure class="p-3 mb-0">
+                        <blockquote class="blockquote">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer mb-0 text-muted">
+                            <cite title="Source Title">Cliente anónimo</cite>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card p-3">
+                    <figure class="p-3 mb-0">
+                        <blockquote class="blockquote">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer mb-0 text-muted">
+                            <cite title="Source Title">Cliente anónimo</cite>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card p-3">
+                    <figure class="p-3 mb-0">
+                        <blockquote class="blockquote">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer mb-0 text-muted">
+                            <cite title="Source Title">Cliente anónimo</cite>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'FeedBacksClients'
+}
+
+</script>
