57fbf99db8ae0164ad357e91208122488435c7a3
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-08-19 11:56:09 -0600

    New component to show main cards

---
+++ src/components/inicio/CardsTienda.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="card_store">
+
+        <div class="row text-center">
+            <div class="col-lg-6">
+                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+                <h2>Construcción</h2>
+                <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                <p><router-link class="btn | btn-warning | m-3" :to="{name: 'construccion'}">Ir al catálogo 🛍️</router-link></p>
+            </div>
+
+            <div class="col-lg-6">
+                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+                <h2>Veterinaria</h2>
+                <p>And lastly this, the third column of representative placeholder content.</p>
+                <p><router-link class="btn | btn-warning | m-3" :to="{name: 'veterinaria'}">Ir al catálogo 🛍️</router-link></p>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CardsStore'
+}
+</script>
--- src/components/inicio/Inicio.vue
+++ src/components/inicio/Inicio.vue
@@ -4,16 +4,8 @@
             <Carousel></Carousel>
         </div>
 
-        <div class="row | mt-5 | m-1 | border | border-success | rounded">
-            <div class="col-12 | col-md-10">
-                <h3 class="d-inline">¿Necesitas materiales para construcción? Visita nuestro catálogo 🛍️</h3>
-                <router-link class="btn | btn-warning | m-3" :to="{name: 'inicio'}">Ir al catálogo de materiales 🛍️</router-link>
-            </div>
-
-            <div class="col-12 | col-md-10">
-                <h3 class="d-inline">¿Necesitas productos veterinarios? Visita nuestro catálogo 🛍️</h3>
-                <router-link class="btn | btn-warning | m-3" :to="{name: 'inicio'}">Ir al catálogo de veterinaria 🛍️</router-link>
-            </div>
+        <div class="row | mt-5 | m-1">
+            <CardsStore></CardsStore>    
         </div>
 
     </div>
@@ -21,11 +13,13 @@
 
 <script>
 import Carousel from './Carousel.vue'
+import CardsStore from './CardsTienda.vue'
 
 export default {
     name: 'Inicio',
     components: {
-        Carousel
+        Carousel,
+        CardsStore
     }
 }
 </script>
