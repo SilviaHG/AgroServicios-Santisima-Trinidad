ff54fd0486c47394717aa25e27e12819597d3d4c
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-08-20 01:12:51 -0600

    Mejora de la presentacion inicial

--- src/components/inicio/Inicio.vue
+++ src/components/inicio/Inicio.vue
@@ -5,12 +5,41 @@
         </div>
 
         <div class="row | mt-5 | m-1">
+            <h2 class="mb-4">~ Catálogo De Productos</h2>
             <CardsStore></CardsStore>
         </div>
 
         <div class="row | mt-5 | m-1">
             <h2>~ Testimonios</h2>
             <FeedBacksClients></FeedBacksClients>
+        </div>
+
+        <div class="row | mt-5 | m-2">
+            <div class="col-8 | offset-md-3">
+                <div class="card | float-end">
+                    <div class="card-body">
+                        <h2 class="card-title">¿Desea contactarnos?</h2>
+                        <p class="card-text">Será un gusto atenderle, por ello estamos disponibles en el siguiente horario:</p>
+
+                        <ul class="list-group | m-2">
+                            <li class="list-group-item d-flex justify-content-between align-items-start active">
+                                <div class="ms-2 me-auto">
+                                <div class="fw-bold">Lunes a Sábado</div>
+                                    7:00 am hasta 7:00 pm
+                                </div>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-start active">
+                                <div class="ms-2 me-auto">
+                                <div class="fw-bold">Domingo</div>
+                                    7:00 am hasta 1:00 pm
+                                </div>
+                            </li>
+                        </ul>
+
+                        <router-link class="btn | btn-success" aria-current="page" :to="{name: 'contactenos'}">Contáctenos aquí 📲</router-link>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
